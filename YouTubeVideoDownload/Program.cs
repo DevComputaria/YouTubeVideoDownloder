10e3b2b48d520fd4056b8fc1443cb8f5a15718bc
cleilson pereira <marcialwushu@hotmail.com>
2021-05-24 22:51:19 -0300

    Add project files.

---
+++ YouTubeVideoDownload/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace YouTubeVideoDownload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // display texto de introdução
+            DisplayIntro();
+        }
+
+        /// <summary>
+        /// Exibe informações do autor e links 
+        /// </summary>
+        static void DisplayIntro()
+        {
+            Console.Title = "YouTube Video Downloader CLI";
+            Console.WriteLine("=========================================");
+            Console.WriteLine("=     Youtube Video Downloader CLI      =");
+            Console.WriteLine("=             DevComputaria             =");
+            Console.WriteLine("=    https://github.com/DevComputaria   =");
+            Console.WriteLine("=========================================");
+        }
+    }
+}
