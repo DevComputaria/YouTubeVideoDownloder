bac1cd88308fd05f91c20349d585f02e19b61bca
cleilson pereira <marcialwushu@hotmail.com>
2021-05-24 23:13:31 -0300

    :fire: Adicionado classe para Tratamento de string

---
+++ YouTubeVideoDownload/Extensions/StringExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace YouTubeVideoDownload.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string GenerateSlug(this string phrase)
+        {
+            string str = phrase.RemoveAccent().ToLower();
+            // char invalido
+            str = Regex.Replace(str, @"[^a-z0-9\s-]", "");
+            // converte multiplos espaços em um unico espaço
+            str = Regex.Replace(str, @"\s+", " ").Trim();
+            // cut and trim 
+            str = str.Substring(0, str.Length <= 45 ? str.Length : 45).Trim();
+            str = Regex.Replace(str, @"\s", "-"); // hifem
+            return str;
+        }
+
+        public static string RemoveAccent(this string txt)
+        {
+            byte[] bytes = Encoding.GetEncoding("Cyrillic").GetBytes(txt);
+            return Encoding.ASCII.GetString(bytes);
+        }
+    }
+}
