bc2961b4434b675823754bae559714f4e395586f
cleilson pereira <marcialwushu@hotmail.com>
2021-05-29 00:19:00 -0300

    Adicionado Utilities da ProgressIndicator

---
+++ YouTubeVideoDownload/Utilities/ProgressIndicator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace YouTubeVideoDownload.Utilities
+{
+    public class ProgressIndicator : IProgress<double>, IDisposable
+    {
+        private readonly int _positionX;
+        private readonly int _positionY;
+
+        public ProgressIndicator()
+        {
+            _positionX = Console.CursorLeft;
+            _positionY = Console.CursorTop;
+        }
+
+        public void Report(double progress)
+        {
+            Console.SetCursorPosition(_positionX, _positionY);
+            Console.WriteLine($"{progress:P1}");
+        }
+        public void Dispose()
+        {
+            Console.SetCursorPosition(_positionX, _positionY);
+        }
+
+    }
+}
